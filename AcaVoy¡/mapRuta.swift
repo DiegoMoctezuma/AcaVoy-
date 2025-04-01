654a56e84ba473567bc1b67a39b674cf3e95443a
NoFreeTime <moctezumadiego5@gmail.com>
2025-04-01 13:26:40 -0600

    finalVersion

---
+++ AcaVoy¡/mapRuta.swift
@@ -0,0 +1,18 @@
+//
+//  mapRuta.swift
+//  AcaVoy¡
+//
+//  Created by MAC13 on 01/04/25.
+//
+
+import SwiftUI
+
+struct mapRuta: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapRuta()
+}
---
+++ AcaVoy¡/menuRuta.swift
@@ -0,0 +1,18 @@
+//
+//  menuRuta.swift
+//  AcaVoy¡
+//
+//  Created by MAC13 on 01/04/25.
+//
+
+import SwiftUI
+
+struct menuRuta: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    menuRuta()
+}
